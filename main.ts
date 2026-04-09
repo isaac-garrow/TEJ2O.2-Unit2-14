40d1d5936a1fe916dc7f6729bb9b0a6cc0da06b5
isaac-garrow <isaac.garrow@stu.ocsb.ca>
2026-04-09 12:44:32 -0400

    changed the programs loop action

--- main.ts
+++ main.ts
@@ -1,8 +1,49 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Isaac Garrow
+ * Created on: Apr 2026
+ * This program shows while loops in action
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// when "A" is pressed, the pixels move down in a diagnol
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(500)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// when "B" is pressed, the pixels move around the outside edge of the LED matrix
+input.onButtonPressed(Button.B, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 5
+    sprite = game.createSprite(5, 5)
+
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(500)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
